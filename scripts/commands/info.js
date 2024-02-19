7ca4b8b774ed83f02414c77882082cfeeeb589c8
mahmudakash3 <124241452+mahmudakash3@users.noreply.github.com>
2024-02-19 12:17:21 +0600

    Update info.js

--- scripts/commands/info.js
+++ scripts/commands/info.js
@@ -1,5 +1,5 @@
 module.exports.config = {
-    name: "admin",
+    name: "info",
     version: "1.0.0",
     permission: 0,
     credits: "nayan",
@@ -28,19 +28,19 @@ var juswa = moment.tz("Asia/Dhaka").format("『D/MM/YYYY』 【hh:mm:ss】");
   
 var callback = () => api.sendMessage({body:`
 --------------------------------------------
-𝐍𝐚𝐦𝐞       : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝐌𝐨𝐡𝐚𝐦𝐦𝐚𝐝 𝐍𝐚𝐲𝐚𝐧
+𝐍𝐚𝐦𝐞       : 𝖠𝗄𝖺𝗌𝗁 𝗆𝖺𝗁𝗆𝗎𝖽
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 : 𝖠𝗄𝖺𝗌𝗁 𝗆𝖺𝗁𝗆𝗎𝖽
 𝐑𝐞𝐥𝐢𝐠𝐢𝐨𝐧   : 𝐈𝐬𝐥𝐚𝐦
-𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐓𝐚𝐧𝐠𝐚𝐢𝐥, 𝐃𝐡𝐚𝐤𝐚
-𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝐌𝐨𝐲𝐦𝐨𝐧𝐬𝐢𝐧𝐡, 𝐃𝐡𝐚𝐤𝐚 𝐁𝐲𝐩𝐚𝐬𝐬
+𝐏𝐞𝐫𝐦𝐚𝐧𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝖣𝗎𝗋𝗀𝖺𝗉𝗎𝗋, 𝖱𝖺𝗃𝗌𝗁𝖺𝗁𝗂
+𝐂𝐮𝐫𝐫𝐞𝐧𝐭 𝐀𝐝𝐝𝐫𝐞𝐬𝐬: 𝖱𝖺𝗃𝗌𝗁𝖺𝗁𝗂, 𝖣𝗎𝗋𝗀𝖺𝗉𝗎𝗋 𝐁𝐲𝐩𝐚𝐬𝐬
 𝐆𝐞𝐧𝐝𝐞𝐫.   : 𝐌𝐚𝐥𝐞
 𝐀𝐠𝐞           : 𝟏𝟖+
 𝐑𝐞𝐥𝐚𝐭𝐢𝐨𝐧𝐬𝐡𝐢𝐩 : 𝐒𝐢𝐧𝐠𝐥𝐞
 𝐖𝐨𝐫𝐤        : 𝐒𝐭𝐮𝐝𝐞𝐧𝐭
-𝐆𝐦𝐚𝐢𝐥       : mohammadnayan447@gmail.com
-𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801615298449
-𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/MOHAMMADNAYAN
-𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/www.xnxx.com169`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
+𝐆𝐦𝐚𝐢𝐥       : 𝗆𝖺𝗁𝗆𝗎𝖽𝖺𝗄𝖺𝗌𝗁309@𝗀𝗆𝖺𝗂𝗅.𝖼𝗈𝗆
+𝐖𝐡𝐚𝐭𝐬𝐀𝐩𝐩: wa.me/+8801706282378
+𝐓𝐞𝐥𝐞𝐠𝐫𝐚𝐦  : t.me/𝖠𝗄𝖺𝗌𝗁𝗆𝖺𝗁𝗆𝗎𝖽
+𝐅𝐚𝐜𝐞𝐛𝐨𝐨𝐤 𝐋𝐢𝐧𝐤 : https://www.facebook.com/www.Its.ME.SIMPLE.CYBER.EXPERTS.AKASH.VAU`,attachment: fs.createReadStream(__dirname + "/cache/1.png")}, event.threadID, () => 
     fs.unlinkSync(__dirname + "/cache/1.png"));  
       return request(encodeURI(`https://graph.facebook.com/100000959749712/picture?height=720&width=720&access_token=6628568379%7Cc1e620fa708a1d5696fb991c1bde5662`)).pipe(
 fs.createWriteStream(__dirname+'/cache/1.png')).on('close',() => callback());
